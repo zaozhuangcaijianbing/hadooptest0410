d332ef4f1dd60e0f99b289fad5dba375e135e6f4
wangning <774901016@qq.com>
2018-05-13 20:06:09 +0800

    update

--- src/main/java/com/wn/hbase/HbaseAddTest.java
+++ src/main/java/com/wn/hbase/HbaseAddTest.java
@@ -1,5 +1,6 @@
 package com.wn.hbase;
 
+import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
@@ -8,6 +9,10 @@ import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HbaseAddTest {
     public static void main(String[] args) throws Exception {
@@ -20,17 +25,42 @@ public class HbaseAddTest {
         Connection connection = ConnectionFactory.createConnection(cfg);
 
         Table table = connection.getTable(TableName.valueOf("wn_test"));
-        while (true) {
-            Put put = new Put(Bytes.toBytes("" + System.currentTimeMillis()));
 
-            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("info"),
-                    Bytes.toBytes("abcdefghijklmnopqrstuvwxyz" + System.currentTimeMillis()));
-            put.addColumn(Bytes.toBytes("base"), Bytes.toBytes("name"),
-                    Bytes.toBytes("yoho" + System.currentTimeMillis()));
-            put.addColumn(Bytes.toBytes("base"), Bytes.toBytes("address"),
-                    Bytes.toBytes("nanjing" + System.currentTimeMillis()));
-            table.put(put);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    List<Put> list = Lists.newArrayList();
+
+                    while (true) {
+                        try {
+
+                            Put put = new Put(Bytes.toBytes(Thread.currentThread().getName() + ":" + System.currentTimeMillis()));
+
+                            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("info"),
+                                    Bytes.toBytes("abcdefghijklmnopqrstuvwxyzasdfasdfasdfasdfadsfasdfasdhjghjhhgjghfasdfasdfaasdfasdfaadsfasdfadfasdfadfasdfasdfasdf" + System.currentTimeMillis()));
+                            put.addColumn(Bytes.toBytes("base"), Bytes.toBytes("name"),
+                                    Bytes.toBytes("yohoasdfasdfasdfasdfasdfasdfsdfasdfasfasdfasdfasdfasdfaadsfadfagfhfghfghfghsdfadsfasdfsdfadfasdfasdf" + System.currentTimeMillis()));
+                            put.addColumn(Bytes.toBytes("base"), Bytes.toBytes("address"),
+                                    Bytes.toBytes("nanjingasdfasdfasdfadfasdfasdfadfasdfasdghjgjghjghjfasdfasdfadfasdfasdfasdfadsfadfadfasdfasdfasdfasdfadfadfasdfasdfadfa" + System.currentTimeMillis()));
+                            list.add(put);
+
+                            if(list.size()>160){
+                                table.put(list);
+                                list = Lists.newArrayList();
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
         }
+
+        executorService.shutdown();
 
     }
 
