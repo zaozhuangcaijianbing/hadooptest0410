8b7aba1ce4e1fcfeda50ebbab7441c4ed7ab082d
wangning <774901016@qq.com>
2020-09-10 15:22:24 +0800

    sparksql

--- src/main/java/com/wn/spark/SparkSqlTest.java
+++ src/main/java/com/wn/spark/SparkSqlTest.java
@@ -1,9 +1,15 @@
 package com.wn.spark;
 
+import org.apache.spark.api.java.function.ForeachPartitionFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Iterator;
 
 import static org.apache.spark.sql.functions.col;
 
@@ -18,20 +24,21 @@ public class SparkSqlTest {
         //分区读取mysql数据
         Dataset<Row> jdbcDF = spark.read()
                 .format("jdbc")
-                .option("driver","com.mysql.jdbc.Driver")
+                .option("driver", "com.mysql.jdbc.Driver")
                 .option("url", "jdbc:mysql://10.142.98.225:2171/QDAM_CONFIG_DEMO?useUnicode=true&characterEncoding=utf8&autoReconnect=true&rewriteBatchedStatements=TRUE")
                 .option("dbtable", "wnmysql0904")
+//                .option("query","select test1,test2,test3 from wnmysql0904 limit 10000")  //dbtable和query不能同时设置
                 .option("user", "QDAM_CONFIG_DEMO")
                 .option("password", "3b9a06826b008e71")
-                .option("numPartitions",4)
-                .option("partitionColumn","id")
-                .option("lowerBound",1)
-                .option("upperBound",5000000)
+                .option("numPartitions", 4)
+                .option("partitionColumn", "id")
+                .option("lowerBound", 1)
+                .option("upperBound", 5000000)
                 .load();
 
 //        Dataset<Row> select = jdbcDF.filter(col("test2").equalTo("Bob"));
-//        Dataset<Row> cache = jdbcDF.cache();
 
+        //写入mysql，方式1
         jdbcDF.write()
                 .format("jdbc")
                 .option("driver","com.mysql.jdbc.Driver")
@@ -42,8 +49,34 @@ public class SparkSqlTest {
                 .mode(SaveMode.Overwrite)
                 .save();
 
+        //写入mysql，方式2
+//        jdbcDF.foreachPartition(new ForeachPartitionFunction<Row>() {
+//
+//            @Override
+//            public void call(Iterator<Row> t) throws Exception {
+//                Class.forName("com.mysql.jdbc.Driver");
+//                Connection conn = DriverManager.getConnection("jdbc:mysql://10.142.98.225:2171/QDAM_CONFIG_DEMO?useUnicode=true&characterEncoding=utf8&autoReconnect=true&rewriteBatchedStatements=TRUE", "QDAM_CONFIG_DEMO", "3b9a06826b008e71");
+//                conn.setAutoCommit(false);
+//                PreparedStatement cmd = conn.prepareStatement("insert into wnmysql0905 (test1,test2,test3) values (?,?,?)");
+//                long start = System.currentTimeMillis() / 1000;
+//                while (t.hasNext()) {
+//                    Row next = t.next();
+//                    String test1 = next.getString(0);
+//                    String test2 = next.getString(1);
+//                    String test3 = next.getString(2);
+//
+//                    cmd.setString(1, test1);
+//                    cmd.setString(2, test2);
+//                    cmd.setString(3, test3);
+//                    cmd.addBatch();
+//                }
+//                cmd.executeBatch();
+//                conn.commit();
+//                long end = System.currentTimeMillis() / 1000;
+//                System.out.println("耗时：" + (end - start));
+//            }
+//        });
 
-        long count = 0L;
-        System.out.println("count:" + count);
+
     }
 }
